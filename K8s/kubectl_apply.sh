171e12506fb6b056cbbc5bde6da63a8be86ed3bd
ebenhamu <ebenhamu@gmail.com>
2025-03-19 11:17:38 +0200

    new

---
+++ K8s/kubectl_apply.sh
@@ -0,0 +1,26 @@
+# FE deployment
+kubectl apply -f fe.yaml
+
+# FE load balancer 
+kubectl apply -f frontend-service.yaml
+
+# BE deployment
+kubectl apply -f be.yaml  
+
+# Persistent Volume Claim
+kubectl apply -f pvc.yaml  
+
+# ConfigMap for SQL Initialization Script
+kubectl apply -f configmap.yaml
+
+# PostgreSQL Deployment
+kubectl apply -f deployment.yaml
+
+# DB Service
+kubectl apply -f service.yaml
+
+# get info 
+kubectl get all
+
+# start tunnel to get external IP for the cluster 
+minikube tunnel
