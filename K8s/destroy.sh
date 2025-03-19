a441f302ea405ad5af2ceec87ccd2b222a78c898
MoniTHOR--Project <46280154+ebenhamu@users.noreply.github.com>
2025-03-19 18:13:00 +0200

    Create destroy.sh

---
+++ K8s/destroy.sh
@@ -0,0 +1,7 @@
+echo "***************** Get all *********************"
+kubectl get all
+echo "*************** Destroying ********************"
+kubectl delete service --all
+kubectl delete deployment --all
+echo "***************** Get all *********************"
+kubectl get all
